3cec9139dac1c09e368d92a76c00668d4915c5af
isfegu <israelfenor@protonmail.com>
2022-07-08 19:31:11 +0000

    style: apply prettier rules

--- tests/e2e/themes.spec.ts
+++ tests/e2e/themes.spec.ts
@@ -1,47 +1,43 @@
 import { test, expect } from '@playwright/test';
 import { locales, defaultLocale } from './helpers/locales';
 
-test.describe.parallel(
-  'Open theme menu in home page and click dark mode to set page to corresponding dark theme',
-  () => {
-    locales.forEach(function (locale: string) {
-      test(locale.toLocaleUpperCase(), async ({ page }) => {
-        let url = process.env.E2E_START_URL ?? 'http://localhost:3000';
-        if (defaultLocale != locale) {
-          url = url + '/' + locale;
-        }
+test.describe
+  .parallel('Open theme menu in home page and click dark mode to set page to corresponding dark theme', () => {
+  locales.forEach(function (locale: string) {
+    test(locale.toLocaleUpperCase(), async ({ page }) => {
+      let url = process.env.E2E_START_URL ?? 'http://localhost:3000';
+      if (defaultLocale != locale) {
+        url = url + '/' + locale;
+      }
 
-        await page.goto(url);
-        await page.click('#theme-options');
-        await page.click('#dark-option');
+      await page.goto(url);
+      await page.click('#theme-options');
+      await page.click('#dark-option');
 
-        const locator = page.locator('html');
-        await expect
-          .soft(locator)
-          .toHaveAttribute('data-theme', 'dark', { timeout: 3000 });
-      });
+      const locator = page.locator('html');
+      await expect
+        .soft(locator)
+        .toHaveAttribute('data-theme', 'dark', { timeout: 3000 });
     });
-  }
-);
+  });
+});
 
-test.describe.parallel(
-  'Open theme menu in home page and click light mode to set page to corresponding light theme',
-  () => {
-    locales.forEach(function (locale: string) {
-      test(locale.toLocaleUpperCase(), async ({ page }) => {
-        let url = process.env.E2E_START_URL ?? 'http://localhost:3000';
-        if (defaultLocale != locale) {
-          url = url + '/' + locale;
-        }
+test.describe
+  .parallel('Open theme menu in home page and click light mode to set page to corresponding light theme', () => {
+  locales.forEach(function (locale: string) {
+    test(locale.toLocaleUpperCase(), async ({ page }) => {
+      let url = process.env.E2E_START_URL ?? 'http://localhost:3000';
+      if (defaultLocale != locale) {
+        url = url + '/' + locale;
+      }
 
-        await page.goto(url);
-        await page.click('#theme-options');
-        await page.click('#light-option');
-        const locator = page.locator('html');
-        await expect
-          .soft(locator)
-          .toHaveAttribute('data-theme', 'light', { timeout: 3000 });
-      });
+      await page.goto(url);
+      await page.click('#theme-options');
+      await page.click('#light-option');
+      const locator = page.locator('html');
+      await expect
+        .soft(locator)
+        .toHaveAttribute('data-theme', 'light', { timeout: 3000 });
     });
-  }
-);
+  });
+});
